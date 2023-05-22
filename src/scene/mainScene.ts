fe379e0618b48310b7f96686674541fbc5c72b95
rabbitSyrup <aswxec@gmail.com>
2023-05-22 16:57:50 +0900

    중간 저장

--- src/scene/mainScene.ts
+++ src/scene/mainScene.ts
@@ -9,22 +9,23 @@ export class MainScene extends Phaser.Scene {
 
   preload(): void {
     // map infomation
-    this.load.tilemapTiledJSON('map',"assets/map/map.json");
+    //this.load.tilemapTiledJSON('map',"assets/map/map.json");
+    this.load.tilemapTiledJSON('house',"assets/map/house.json");
 
     // tilesets
-    this.load.spritesheet('Fences','assets/images/tilesets/Fences.png', { frameWidth: 16, frameHeight: 16});
-    this.load.spritesheet('Grass hill tiles v.2','assets/images/tilesets/Grass hill tiles v.2.png', { frameWidth: 16, frameHeight: 16});
-    this.load.spritesheet('Grass tiles v.2','assets/images/tilesets/Grass tiles v.2.png', { frameWidth: 16, frameHeight: 16});
-    this.load.spritesheet('Grass','assets/images/tilesets/Grass.png', { frameWidth: 16, frameHeight: 16});
-    this.load.spritesheet('Hills','assets/images/tilesets/Hills.png', { frameWidth: 16, frameHeight: 16});
-    this.load.spritesheet('Tall Grass hill tiles v.2','assets/images/tilesets/Tall Grass hill tiles v.2.png', { frameWidth: 16, frameHeight: 16});
-    this.load.spritesheet('Tilled Dirt','assets/images/tilesets/Tilled Dirt.png', { frameWidth: 16, frameHeight: 16});
-    this.load.spritesheet('Water','assets/images/tilesets/Water.png', { frameWidth: 16, frameHeight: 16});
+    // this.load.spritesheet('Fences','assets/images/tilesets/Fences.png', { frameWidth: 16, frameHeight: 16});
+    // this.load.spritesheet('Grass hill tiles v.2','assets/images/tilesets/Grass hill tiles v.2.png', { frameWidth: 16, frameHeight: 16});
+    // this.load.spritesheet('Grass tiles v.2','assets/images/tilesets/Grass tiles v.2.png', { frameWidth: 16, frameHeight: 16});
+    // this.load.spritesheet('Grass','assets/images/tilesets/Grass.png', { frameWidth: 16, frameHeight: 16});
+    // this.load.spritesheet('Hills','assets/images/tilesets/Hills.png', { frameWidth: 16, frameHeight: 16});
+    // this.load.spritesheet('Tall Grass hill tiles v.2','assets/images/tilesets/Tall Grass hill tiles v.2.png', { frameWidth: 16, frameHeight: 16});
+    // this.load.spritesheet('Tilled Dirt','assets/images/tilesets/Tilled Dirt.png', { frameWidth: 16, frameHeight: 16});
+    // this.load.spritesheet('Water','assets/images/tilesets/Water.png', { frameWidth: 16, frameHeight: 16});
     this.load.spritesheet('Wooden House','assets/images/tilesets/Wooden House.png', { frameWidth: 16, frameHeight: 16});
 
     // objects
     this.load.spritesheet('Basic Furniture', 'assets/images/objects/Basic Furniture.png', { frameWidth: 16, frameHeight: 16});
-    this.load.spritesheet('Basic Grass Biom things 1', 'assets/images/objects/Basic Grass Biom things 1.png', { frameWidth: 16, frameHeight: 16});
+    // this.load.spritesheet('Basic Grass Biom things 1', 'assets/images/objects/Basic Grass Biom things 1.png', { frameWidth: 16, frameHeight: 16});
 
     // player
     this.load.spritesheet('player', 'assets/images/characters/Basic Character Spritesheet.png', { frameWidth: 16, frameHeight: 16, margin: 16, spacing: 32 });
@@ -39,55 +40,108 @@ export class MainScene extends Phaser.Scene {
     this.anims.create({key: "player-right", frames: this.anims.generateFrameNumbers("player", {start: 12, end: 15, first: 14}), frameRate: 5, repeat: -1});
     this.anims.create({key: "player-down", frames: this.anims.generateFrameNumbers("player", {start: 0, end: 3, first: 2}), frameRate: 5, repeat: -1});
 
+    // door
+    this.anims.create({key: "door-open", frames: this.anims.generateFrameNumbers("Wooden House", {frames: [10, 24, 17], first: 10}), frameRate: 5, repeat: 0});
+
     // map
-    const map = this.make.tilemap({ key: "map" });
+    // const map = this.make.tilemap({ key: "map" });
+    const house = this.make.tilemap({ key: "house" });
     const tilesets = [];
-    tilesets.push(map.addTilesetImage("Fences", "Fences"));
-    tilesets.push(map.addTilesetImage("Grass hill tiles v.2", "Grass hill tiles v.2"));
-    tilesets.push(map.addTilesetImage("Grass tiles v.2", "Grass tiles v.2"));
-    tilesets.push(map.addTilesetImage("Grass", "Grass"));
-    tilesets.push(map.addTilesetImage("Hills", "Hills"));
-    tilesets.push(map.addTilesetImage("Tall Grass hill tiles v.2", "Tall Grass hill tiles v.2"));
-    tilesets.push(map.addTilesetImage("Tilled Dirt", "Tilled Dirt"));
-    tilesets.push(map.addTilesetImage("Water", "Water"));
-    tilesets.push(map.addTilesetImage("Wooden House", "Wooden House"));
+    // tilesets.push(map.addTilesetImage("Fences", "Fences"));
+    // tilesets.push(map.addTilesetImage("Grass hill tiles v.2", "Grass hill tiles v.2"));
+    // tilesets.push(map.addTilesetImage("Grass tiles v.2", "Grass tiles v.2"));
+    // tilesets.push(map.addTilesetImage("Grass", "Grass"));
+    // tilesets.push(map.addTilesetImage("Hills", "Hills"));
+    // tilesets.push(map.addTilesetImage("Tall Grass hill tiles v.2", "Tall Grass hill tiles v.2"));
+    // tilesets.push(map.addTilesetImage("Tilled Dirt", "Tilled Dirt"));
+    // tilesets.push(map.addTilesetImage("Water", "Water"));
+    // tilesets.push(map.addTilesetImage("Wooden House", "Wooden House"));
+    tilesets.push(house.addTilesetImage("Wooden House", "Wooden House"));
 
     // objects
-    tilesets.push(map.addTilesetImage("Basic Furniture", "Basic Furniture"));
-    tilesets.push(map.addTilesetImage("Basic Grass Biom things 1", "Basic Grass Biom things 1"));
+    tilesets.push(house.addTilesetImage("Basic Furniture", "Basic Furniture"));
+    // tilesets.push(map.addTilesetImage("Basic Furniture", "Basic Furniture"));
+    // tilesets.push(map.addTilesetImage("Basic Grass Biom things 1", "Basic Grass Biom things 1"));
 
     // layer
-    const terrainLayer = map.createLayer("terrain", tilesets, 0, 0) as Phaser.Tilemaps.TilemapLayer;
-    terrainLayer.setCollisionByProperty({ collides: true });
-    const groundCollide = map.createLayer("ground-collide", tilesets, 0, 0) as Phaser.Tilemaps.TilemapLayer;
-    groundCollide.setCollisionByProperty({ collides: true });
-    const groundOverlap = map.createLayer("ground-overlap", tilesets, 0, 0) as Phaser.Tilemaps.TilemapLayer;
-    const skyLayer = map.createLayer("sky", tilesets, 0, 0) as Phaser.Tilemaps.TilemapLayer;
+    // layer from tilemap
+    // const terrainLayer = map.createLayer("terrain", tilesets, 0, 0) as Phaser.Tilemaps.TilemapLayer;
+    // terrainLayer.setCollisionByProperty({ collides: true });
+    // const groundCollide = map.createLayer("ground-collide", tilesets, 0, 0) as Phaser.Tilemaps.TilemapLayer;
+    // groundCollide.setCollisionByProperty({ collides: true });
+    // const groundOverlap = map.createLayer("ground-overlap", tilesets, 0, 0) as Phaser.Tilemaps.TilemapLayer;
+    // const skyLayer = map.createLayer("sky", tilesets, 0, 0) as Phaser.Tilemaps.TilemapLayer;
+    const floorLayer = house.createLayer("floor", tilesets, 0, 0) as Phaser.Tilemaps.TilemapLayer;
+    const wallLayer = house.createLayer("wall", tilesets, 0, 0) as Phaser.Tilemaps.TilemapLayer;
+    wallLayer.setCollisionByProperty({ collides: true });
 
-    map.createFromObjects("object", {name: "door"}, true);
-
+    // dynamic layer
     const dynamicLayer = this.add.layer();
-    terrainLayer.setDepth(0);
-    groundCollide.setDepth(1);
-    groundOverlap.setDepth(2);
-    dynamicLayer.setDepth(3);
-    skyLayer.setDepth(4);
+
+    // layer depth setting
+    // terrainLayer.setDepth(0);
+    // groundCollide.setDepth(1);
+    // groundOverlap.setDepth(2);
+    // dynamicLayer.setDepth(3);
+    // skyLayer.setDepth(4);
 
     // player
-    const playerPoint = map.findObject("object", obj => obj.name === "player") as Phaser.Types.Tilemaps.TiledObject;
+    // const playerPoint = map.findObject("object", obj => obj.name === "player") as Phaser.Types.Tilemaps.TiledObject;
+    const playerPoint = house.findObject("object", obj => obj.name === "player") as Phaser.Types.Tilemaps.TiledObject;
     this.player = this.physics.add.sprite(playerPoint.x as number, playerPoint.y as number, "player").play("player-down");
-    this.player.setSize(16, 8);
+    this.player.setSize(12, 16);
     this.playerDirection = "down";
 
     dynamicLayer.add(this.player);
-    this.physics.add.collider(this.player, terrainLayer);
-    this.physics.add.collider(this.player, groundCollide);
+    // this.physics.add.collider(this.player, terrainLayer);
+    // this.physics.add.collider(this.player, groundCollide);
+    this.physics.add.collider(this.player, wallLayer);
 
-    // keyboard settings
+    // doors
+    // const houseDoorPoint = map.findObject("object", obj => obj.name === "house door") as Phaser.Types.Tilemaps.TiledObject;
+    // const houseDoor = this.physics.add.staticSprite(houseDoorPoint.x as number, houseDoorPoint.y as number, "Wooden House", 10);
+    // const houseDoorCollider = this.physics.add.collider(this.player, houseDoor, (object1: any, object2: any) => {
+    //   let door;
+    //   if(object1.texture.key == "player") {
+    //     door = object2;
+    //   } else {
+    //     door = object1;
+    //   }
+
+    //   this.openDoor(door, houseDoorCollider);
+    // });
+    // const roomDoorPoint = map.findObject("object", obj => obj.name === "room door") as Phaser.Types.Tilemaps.TiledObject;
+    // const roomDoor = this.physics.add.staticSprite(roomDoorPoint.x as number, roomDoorPoint.y as number, "Wooden House", 10);
+    // const roomDoorCollider = this.physics.add.collider(this.player, roomDoor, (object1: any, object2: any) => {
+    //   let door;
+    //   if(object1.texture.key == "player") {
+    //     door = object2;
+    //   } else {
+    //     door = object1;
+    //   }
+
+    //   this.openDoor(door, roomDoorCollider);
+    // });
+
+    // keyboard setting
     this.cursor = this.input.keyboard.createCursorKeys() as Phaser.Types.Input.Keyboard.CursorKeys;
+
+    // camera setting
+    this.cameras.main.startFollow(this.player, true, 0.09, 0.09);
+    this.cameras.main.setZoom(2);
   }
 
-  update(time: number, delta: number): void {
+  openDoor(door: any, collider: any): void {
+    if(!door.open) {
+      door.play("door-open");
+      door.open = true;
+      setTimeout(() => {
+        this.physics.world.removeCollider(collider);
+      }, 800);
+    }
+  }
+
+  update(): void {
     // Stop any previous movement from the last frame
     this.player.body.setVelocity(0);
 
